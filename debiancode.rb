272a2ea0417100e160f3b37e5d4dd35b3da49c9f
(shinchron) <(suzu12shin28@i.softbank.jp)>
2016-11-15 04:21:20 +0000

    課題

--- debiancode.rb
+++ debiancode.rb
@@ -5,8 +5,19 @@ require 'pp'
 require 'nokogiri'
 
 url = 'https://wiki.debian.org/DebianReleases'
-html = open(url).read
-doc.xpath('//h2')
+html = open(url).read.squeeze("\n")
+
+doc = Nokogiri::HTML.parse(html).xpath('//table').first
+doc = doc.xpath('tbody/tr')
+
+
+doc.each do |tr|
+  tr.css('td').each do |td|
+    text = td.css('p').inner_text.chomp
+    print text.ljust(25)
+  end
+  puts
+end
 
 # puts html
 doc = Nokogiri::HTML.parse(html)
